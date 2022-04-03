b6b8d8417c801513c79077766a55f8d3cd7cced1
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-04-03 08:02:19 +0200

    Created basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Recipes } from './components/recipes-book/recipes.component';
+import { RecipeDetail } from './components/recipes-book/recipe-detail/recipe-detail.component';
+import { RecipeItem } from './components/recipes-book/recipe-item/recipe-item.component';
+import { RecipesList } from './components/recipes-book/recipe-list/recipes-list.component';
+import { ShoppingList } from './components/shopping-list/shopping-list.component';
+import { ShoppingEdit } from './components/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeDetail,
+    RecipeItem,
+    RecipesList,
+    Recipes,
+    ShoppingList,
+    ShoppingEdit,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
