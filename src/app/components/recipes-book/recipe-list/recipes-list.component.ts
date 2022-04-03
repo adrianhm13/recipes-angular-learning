51409cd3740923bdd40f6dbc33969b9406c9aba5
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-04-03 18:31:51 +0200

    Added recipes model

--- src/app/components/recipes-book/recipe-list/recipes-list.component.ts
+++ src/app/components/recipes-book/recipe-list/recipes-list.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
 })
 export class RecipesList {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'a description test',
+      'https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg'
+    ),
+  ];
+
   constructor() {}
 }
