b13b5f147cc53e3c49b65c9923caf5522d279682
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-04-04 09:17:04 +0200

    Added navigation feature with ngIf

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() changeView = new EventEmitter<string>();
   collapsed = true;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(feature: string) {
+    this.changeView.emit(feature);
   }
-
 }
